fcd9ab52ff7c47f6ba7dc9ef11b72a8d53d10f37
AlexNDR <work.afanasenko@gmail.com>
2023-06-05 18:12:59 +0300

    Architecture changes. Also changed update operation

---
+++ Meetup.Core.Application/IMeetupRepository.cs
@@ -0,0 +1,16 @@
+﻿using Meetup.Core.Domain;
+
+namespace Meetup.Core.Application;
+
+public interface IMeetupRepository
+{
+	public Task<bool> CreateAsync(MeetupView meetup, CancellationToken token = default);
+
+	public Task<IEnumerable<MeetupView>> GetAllAsync(CancellationToken token = default);
+
+	public Task<MeetupView> GetByIdAsync(int id, CancellationToken token = default);
+
+	public Task<bool> UpdateAsync(MeetupView meetup, CancellationToken token = default);
+
+	public Task<bool> DeleteAsync(int id, CancellationToken token = default);
+}
---
+++ Meetup.Tests/Usings.cs
@@ -0,0 +1,4 @@
+global using Xunit;
+global using Meetup.Core.Domain;
+global using Meetup.Core.Application;
+global using Meetup.Infrastructure;
